73d0b909abc23cfda1212d11a06d8d8b76f69a6e
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-02-17 21:11:53 +0300

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,35 @@
+/*
+  Ödev - Dizideki Elemanları Sıralama
+  Java dilinde, dizideki elemanları küçükten büyüğe sıralayan programı yazınız. Dizinin boyutunu ve dizinin elemanlarını kullanıcıdan alınız.
+*/
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int n, inp;
+        
+        System.out.print("Enter the dimension of your array: ");
+        n = input.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            System.out.print((i + 1) + ". item in the array: ");
+            inp = input.nextInt();
+            arr[i] = inp;
+        }
+        
+        Arrays.sort(arr);
+        
+        for (int i=0; i<n; i++) {
+            System.out.print("The Sorted Array: " + arr[i] + " ");
+        }
+
+    }
+}
